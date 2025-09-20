5939abf0e003c10b5662aac014547641f24331d3
Lukino2772 <luka.shvelidze.2@iliauni.edu.ge>
2025-09-20 17:06:12 +0400

    .

--- src/commands.py
+++ src/commands.py
@@ -19,14 +19,14 @@ def init_db():
 def populate_db():
 
     books = [
-        {"id": 0, "name": "დიდი მოგზაურობა", "price": "32", "img": "book1.png"},
-        {"id": 1, "name": "Amare La vita", "price": "31", "img": "book2.png"},
-        {"id": 2, "name": "ქაოსიდან კოსმოსამდე", "price": "30", "img": "book3.png"},
-        {"id": 3, "name": "იესეს ხის საკითხავები", "price": "29", "img": "book4.png"},
+        {"id": 0, "title": "დიდი მოგზაურობა", "price": "32", "img": "book1.png"},
+        {"id": 1, "title": "Amare La vita", "price": "31", "img": "book2.png"},
+        {"id": 2, "title": "ქაოსიდან კოსმოსამდე", "price": "30", "img": "book3.png"},
+        {"id": 3, "title": "იესეს ხის საკითხავები", "price": "29", "img": "book4.png"},
     ]
 
     for book in books:
-        new_book = Book(name=book["name"], price=book["price"], image=book["img"])
+        new_book = Book(title=book["title"], price=book["price"], image=book["img"])
         db.session.add(new_book)
     db.session.commit()
 
