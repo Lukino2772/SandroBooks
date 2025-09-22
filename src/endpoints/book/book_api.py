4c5b38f0f2a15ad037db3ff30d150afd7cc06b63
Lukino2772 <luka.shvelidze.2@iliauni.edu.ge>
2025-09-22 15:56:55 +0400

    .

--- src/endpoints/book/book_api.py
+++ src/endpoints/book/book_api.py
@@ -1,9 +1,9 @@
-from random import choices
-
 from flask_restx import Resource, fields, reqparse
 from src.ext import api, db
 from src.models.book import Book
 from src.enums import CoverType, Booktype, Audience
+
+book_model = api.model('Book', {"id": fields.Integer, "title": fields.String, "image": fields.String})
 
 parser = reqparse.RequestParser()
 parser.add_argument('cover_type', type=str, choices=[e.value for e in CoverType], required=False)
@@ -14,6 +14,31 @@ parser.add_argument("sort", type=int, required=False)
 
 @api.route("/books")
 class Book(Resource):
+    @api.expect(book_model, validate=True)
+    @api.marshal_list_with(book_model)
     def get(self):
-        book = Book.query.all()
-        return book
+        args = parser.parse_args()
+        query = Book.query
+
+        if args["audience"]:
+            query = query.filter(Book.audience == args["audience"])
+        if args["book_type"]:
+            query = query.filter(Book.book_type == args["book_type"])
+        if args["cover_type"]:
+            query = query.filter(Book.cover_type == args["cover_type"])
+        if args["search"]:
+            query = query.filter(Book.title.ilike(f"%{args['search']}%"))
+
+        if args["sort"] == "free":
+            query = query.filter(Book.price == 0)
+        elif args["sort"] == "price_desc":
+            query = query.filter(Book.price.desc())
+        elif args["sort"] == "price_asc":
+            query = query.filter(Book.price.asc())
+        elif args["sort"] == "date_desc":
+            query = query.filter(Book.date.desc())
+        elif args["sort"] == "date_asc":
+            query = query.filter(Book.date.asc())
+
+        book = query.all()
+        return [{"id" : b.id, "title" : b.title, "image" : b.image} for b in book]
