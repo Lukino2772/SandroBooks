ebc6c0a4d5a20d1f65dfa3cc15171af8a121f9bf
Lukino2772 <luka.shvelidze.2@iliauni.edu.ge>
2025-09-22 09:20:11 +0400

    .

--- src/endpoints/book/book_api.py
+++ src/endpoints/book/book_api.py
@@ -1,6 +1,16 @@
-from flask_restx import Resource
-from src.ext import api
+from random import choices
+
+from flask_restx import Resource, fields, reqparse
+from src.ext import api, db
 from src.models.book import Book
+from src.enums import CoverType, Booktype, Audience
+
+parser = reqparse.RequestParser()
+parser.add_argument('cover_type', type=str, choices=[e.value for e in CoverType], required=False)
+parser.add_argument("audience", type=str, choices=[e.value for e in Audience], required=False)
+parser.add_argument("book_type", type=str, choices=[e.value for e in Booktype], required=False)
+parser.add_argument("search", type=str, required=False)
+parser.add_argument("sort", type=int, required=False)
 
 @api.route("/books")
 class Book(Resource):
