a1ad2cc06f670c342058e0f8be7fd955c130bbdf
Lukino2772 <luka.shvelidze.2@iliauni.edu.ge>
2025-09-29 15:44:34 +0400

    .

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,23 @@
 from flask import Flask
+from sqlalchemy import Sequence
+
 from src.admin_views.base import SecureModelView
 from src.config import Config
-from src.ext import db, migrate, admin, api
+from src.ext import db, migrate, login_manager, admin, api
+from src.views import  auth_blueprint, book_blueprint
 from src.commands import init_db, populate_db
 from src.models.user import User
 from src.models.book import Book
 
+BLUEPRINTS = [ auth_blueprint, book_blueprint]
 COMMANDS = [init_db, populate_db]
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     register_extensions(app)
+    register_blueprints(app)
+    register_commands(app)
     return app
 
 def register_extensions(app):
@@ -21,6 +27,14 @@ def register_extensions(app):
     # Flask-Migrate
     migrate.init_app(app, db)
 
+    #Flask-Login
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    @login_manager.user_loader
+    def load_user(_id):
+        return User.query.get(_id)
+
     # Flask_admin
     admin.init_app(app)
     admin.add_view(SecureModelView(User, db.session))
@@ -28,3 +42,11 @@ def register_extensions(app):
 
     #Flask_RestX
     api.init_app(app)
+
+def register_blueprints(app):
+    for blueprint in BLUEPRINTS:
+        app.register_blueprint(blueprint)
+
+def register_commands(app):
+    for command in COMMANDS:
+        app.cli.add_command(command)
--- src/ext.py
+++ src/ext.py
@@ -1,10 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from flask_admin import Admin
 from src.admin_views.base import SecureIndexView
 from flask_restx import Api
 
 db = SQLAlchemy()
 migrate = Migrate()
-admin = Admin(index_view=SecureIndexView())
+login_manager = LoginManager()
+admin = Admin(template_mode="bootstrap4", index_view=SecureIndexView())
 api = Api(title="SB")
